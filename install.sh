9533a507e85245a4b6a4fb3cdd47dd6726f1c15e
Logicer <logicer@logicer.cc>
2024-12-20 21:25:34 +1100

    fix error caused by trailing space

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ( cd /tmp/ && \
 git clone https://github.com/Logicer16/pam-watchid.git && \
-cd "$(basename "$_" .git)" && CLONE_SUCCESS="true" && \ 
+cd "$(basename "$_" .git)" && CLONE_SUCCESS="true" && \
 make $1
 # cleanup
 [[ "$CLONE_SUCCESS" == "true" ]] && TMP=`pwd -P` && cd "`dirname $TMP`" && rm -rf "./`basename $TMP`" && unset TMP
